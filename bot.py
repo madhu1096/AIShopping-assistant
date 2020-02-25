51aa6d2f91970fc76da9c72bcb3dc69af2b2c5d0
Madhusuthanan Manivannan <57857309+madhu1096@users.noreply.github.com>
2020-02-25 18:06:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,13 +20,13 @@ shirt    = pd.read_csv('Shirt.csv',header=None,sep='\t')
 phant    = pd.read_csv('phant.csv',header=None,sep='\t')
 belt     = pd.read_csv('belt.csv',header=None,sep='\t')
 wallet   = pd.read_csv('wallet.csv',header=None,sep='\t')
-watches  = pd.read_csv('watches.csv',header=None,sep='\t')
+watche   = pd.read_csv('watches.csv',header=None,sep='\t')
 
 shirt    = shirt.iloc[:,:].values
 phant    = phant.iloc[:,:].values
 belt     = belt.iloc[:,:].values
 wallet   = wallet.iloc[:,:].values
-watches  = watches.iloc[:,:].values
+watche   = watche.iloc[:,:].values
 
 
 #countvectorizer
@@ -51,18 +51,91 @@ model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metr
 
 model.fit(X,y, batch_size = 32, epochs = 10)
 
+def display_products(pred,chat_id,msg):
+    
+    recent = None
+    if pred == 2:
+        for i in range(len(shirt)):
+            bot.sendMessage(chat_id, shirt[i][1])
+            reply = shirt[i][1]
+            write_function(msg,reply)
+            bot.sendMessage(chat_id, shirt[i][0])
+            reply = str(shirt[i][0])
+            write_function(msg,reply)
+        else:
+            reply = 'Please send me the number which is below the picture to buy'
+            bot.sendMessage(chat_id, reply)
+            write_function(msg,reply)
+            recent = 'shirt'
+    elif pred == 3:
+        for i in range(len(phant)):
+            bot.sendMessage(chat_id, phant[i][1])
+            reply = phant[i][1]
+            write_function(msg,reply)
+            bot.sendMessage(chat_id, phant[i][0])
+            reply = str(phant[i][0])
+            write_function(msg,reply)
+        else:
+            reply = 'Please send me the number which is below the picture to buy'
+            bot.sendMessage(chat_id, reply)
+            write_function(msg,reply)
+            recent = 'phant'
+    elif pred == 4:
+        for i in range(len(belt)):
+            bot.sendMessage(chat_id, belt[i][1])
+            reply = belt[i][1]
+            write_function(msg,reply)
+            bot.sendMessage(chat_id, belt[i][0])
+            reply = str(belt[i][0])
+            write_function(msg,reply)
+        else:
+            reply = 'Please send me the number which is below the picture to buy'
+            bot.sendMessage(chat_id, reply)
+            write_function(msg,reply)
+            recent = 'belt'
+    elif pred == 5:
+        for i in range(len(wallet)):
+            bot.sendMessage(chat_id, wallet[i][1])
+            reply = wallet[i][1]
+            write_function(msg,reply)
+            bot.sendMessage(chat_id, wallet[i][0])
+            reply = str(wallet[i][0])
+            write_function(msg,reply)
+        else:
+            reply = 'Please send me the number which is below the picture to buy'
+            bot.sendMessage(chat_id, reply)
+            write_function(msg,reply)
+            recent = 'wallet'
+    elif pred == 6:
+        for i in range(len(watch)):
+            bot.sendMessage(chat_id, watch[i][1])
+            reply = watch[i][1]
+            write_function(msg,reply)
+            bot.sendMessage(chat_id, watch[i][0])
+            reply = str(watch[i][0])
+            write_function(msg,reply)
+        else:
+            reply = 'Please send me the number which is below the picture to buy'
+            bot.sendMessage(chat_id, reply)
+            write_function(msg,reply)
+            recent = 'watch'
+    return recent
 
-def bot_session(message):        
+def bot_session(message,chat_id,msg,temp):        
    
     ip = message
     ip_1=np.append(ip,1)
     x1 = cv.transform(ip_1).toarray()
     pred = model.predict_classes(x1)
     pred = pred[0]-1
-    op = str(dataset1.iloc[pred,1])
+    print(pred)
+    reply = str(dataset1.iloc[pred,1])
     if ip == 'quit' or ip == 'Quit':
-        op = 'bye'   
-    return op
+        reply = 'bye'   
+    bot.sendMessage(chat_id, reply)
+    write_function(msg,reply)
+    recent = display_products(pred,chat_id,msg)
+    return recent
 
 def write_function(msg,reply):
     
@@ -81,37 +154,32 @@ def write_function(msg,reply):
     file.close()
     
 
+    
 def handle(msg):
+         
     chat_id = msg['chat']['id']
     command = msg['text']
-    message = str(command)
-    print ('Got command: %s' % message)
-    print(msg)
-    if message == 'Picture':
-#       bot.sendPhoto(chat_id, 'https://www.instagram.com/p/B88W3Krp18A/')
-        reply = 'To view more about this.. click the below link'
-        bot.sendMessage(chat_id, reply)
-        reply = 'https://www.instagram.com/p/B88W3Krp18A/'
-        bot.sendMessage(chat_id, reply)
+    if command.isnumeric():
+        temp = 'numeric'
+        message = str(command)
+        print ('Got command: %s' % message)
+        print(msg)
+        recent = bot_session(message,chat_id,msg,temp)
     else:
-        reply = bot_session(message)
-        bot.sendMessage(chat_id, reply)
-    write_function(msg,reply)
+        temp = 'not'
+        message = str(command)
+        print ('Got command: %s' % message)
+        print(msg)
+        recent = bot_session(message,chat_id,msg,temp)
+        
 
-
-
-
+        
+    
 
 bot = telepot.Bot('1070030315:AAG46yVpnlqNMQ4W3Z1g_iYFmtpSJEe1eY0')
-
 first_init = 'hi'
 first_init=np.append(first_init,1)
 x1=cv.transform(first_init).toarray()
 pred=model.predict_classes(x1)
 
-counter = 0 
 bot.message_loop(handle)
-
-
-
- 
